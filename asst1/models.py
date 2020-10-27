38950f3f8d5cdf1bef62a1c2a0ebbd9daf3e7f17
shyamp99 <shyamp99@gmail.com>
2020-10-27 12:28:03 -0400

    cleaned up and commented

--- asst1/models.py
+++ asst1/models.py
@@ -6,36 +6,34 @@ import math
 
 class neuron:
     def __init__(self, vt, time = 100, step = .5):
-        #all time units are milliseconds
+        # all time units are milliseconds
         self.time = time
         self.step = step
         #our spike threshold
         self.vt = vt
         # so simulate, i value in this arr is considered to be 1 step
-        # so like element 0 is t = 0 and element n is t = n*step
+        # so element 0 is t = 0 and element n is t = n*(1/step)
         self.time_arr = np.arange(0, time+1, step)
         # membrane potential for a given time: it has 1:1 correspondence with time_arr
         self.vm = None
         # tells us when spikes via a 1
         self.spikes = np.zeros(int(time/step)+1)
         # just an array to keep track of our voltages to make it easier to plot
-        self.v = np.zeros(int(time/step)+1)
+        self.a = np.zeros(int(time/step)+1)
         
         
-
-    #using plotly to make the graph cuz it's so much fucking better than matplotlib
-    #if mode is 0 we are plotting hodkins huxley and don't need a spike threshold line
+    # if mode is 0 we are plotting hodkins huxley and don't need a spike threshold line
     def plot_graph(self, title, mode = 1):
         vt_line = self.vt*np.ones(int(self.time/self.step)+1)
         fig = go.Figure()
         if mode:
             fig.add_trace(go.Scatter(x=self.time_arr, y=vt_line, mode='lines', name='Spike Threshold', line=dict(color="blue", dash='dot')))
-        fig.add_trace(go.Scatter(x=self.time_arr, y=self.vm, mode='lines', name='Membrane Potential'))
-        fig.add_trace(go.Scatter(x=self.time_arr, y=self.a, mode='lines', name='Input Voltage'))
+        fig.add_trace(go.Scatter(x=self.time_arr, y=self.vm, mode='lines', name='Membrane Potential (mv)'))
+        fig.add_trace(go.Scatter(x=self.time_arr, y=self.a, mode='lines', name='Input Current (a)'))
         fig.update_layout(
             title = title,
             xaxis_title="Time (ms)",
-            yaxis_title="Voltage (mv)"
+            yaxis_title="Value"
         )
         fig.show()
         
@@ -52,11 +50,9 @@ class lif:
         self.input2 = input2
         self.vt = vt
         self.vr = vr
-    
+
+    # we are just solving for dv here
     def current_voltage(self, input, step, prev):
-        # we are just solving for dv here
-        # return self.rm*input - self.tau*input*step
-        # print(((-self.neuron.vm[i-1] + input*self.rm) / self.tau)*step)
         return ((-prev + input*self.rm) / self.tau)
 
     # flip is the time when we want to up voltage: refers to index in time_arr
@@ -67,19 +63,19 @@ class lif:
             prev = self.neuron.vm[i-1] if self.neuron.vm[i-1]<self.vt else self.vr
             if i < flip:
                 mp = prev+self.current_voltage(self.input1, step, prev)
-                self.neuron.v[i] = self.input1
+                self.neuron.a[i] = self.input1
             elif (i>=flip and i <= flip_end):
                 mp = prev+self.current_voltage(self.input2, step, prev)
-                self.neuron.v[i] = self.input2
+                self.neuron.a[i] = self.input2
             elif i > flip_end:
                 mp = prev+self.current_voltage(self.vr, step, prev)
-                self.neuron.v[i] = self.vr
+                self.neuron.a[i] = self.vr
             if mp >= self.vt:
                 self.neuron.spikes[i] = 1
             self.neuron.vm[i] = mp
             
 class izhikevich:
-    # vm is array of membrane potentials at time t (usually starts at -65mv), vt is threshold (usually +30mv), u is membrane recovery variable (i think it's initially v*b but not sure)
+    # vm is array of membrane potentials at time t (usually starts at -65mv), vt is threshold (usually +30mv), u is membrane recovery variable
     # a = time scale of recovery var u - smaller - slower recovery, typically 0.02
     # b = sensitivity of recovery var u - greater  results in "Greater values couple and more strongly resulting in possible subthreshold oscillations and low-threshold spiking dynamics
     # c = reset membrane potential - usually -65 mv
@@ -96,40 +92,36 @@ class izhikevich:
         self.u[0] = self.b*c
         self.neuron.vm[0] = c
 
-    #What i'm missing is how dv and du play together
-    #i'm p sure my logic is sound for this but check it over
-    def simulate(self, input_v, input_v2, flip, flip_end):
+    # the two input_a parameters are the two input amperages
+    # flip, flip_end represents the portion in time_arr where we want ot up the amperage
+    # after flip end the input current is 0
+    def simulate(self, input_a, input_a2, flip, flip_end):
         k = 0
-        inputs = (input_v, input_v2, 0)
+        inputs = (input_a, input_a2, 0)
         reset = False
         temp = 0
         # get_mp = lambda i: self.neuron.vm[i-1]+self.neuron.step*( 0.04*self.neuron.vm[i-1]**2 + 5*self.neuron.vm[i-1] + 140 - self.u[i-1] + inputs[k])
         # get_u = lambda i: self.u[i-1]+self.neuron.step*(self.a*(self.b*curr_v-self.u[i-1]))
         for i in range(1, len(self.neuron.vm)):
-            # print(inputs[k])
             if i == flip:
                 k = 1
             elif i == flip_end:
                 k =2
             curr_v = self.neuron.vm[i-1]+self.neuron.step*( 0.04*self.neuron.vm[i-1]**2 + 5*self.neuron.vm[i-1] + 140 - self.u[i-1] + inputs[k])
-            # print(curr_v)
             self.u[i] = self.u[i-1]+(self.a*(self.b*curr_v-self.u[i-1]))
-            # once neuron hits 30 we spike and then set v to c, u+=d
             if reset:
                 self.neuron.vm[i-1] = temp
                 reset = False
             if curr_v > self.vt:
                 temp = curr_v
                 self.neuron.vm[i] = self.c
-                # print(self.c)
                 self.u[i] += self.d
-                #30 is a placeholder i'll adjust the value according tothe final graph
                 self.neuron.spikes[i] = self.vt
-                self.neuron.v[i] = inputs[k]
+                self.neuron.a[i] = inputs[k]
                 reset = True
                 continue
             self.neuron.vm[i] = curr_v
-            self.neuron.v[i] = inputs[k]
+            self.neuron.a[i] = inputs[k]
 
 class hodgkinhuxley:
     
